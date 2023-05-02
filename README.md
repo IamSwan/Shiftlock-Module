9a130c35974717793b4a78a1fdd5e9460628d6e3
Swan Sarli <swan.sarli@epitech.eu>
2023-05-02 17:45:32 +0200

    added project

---
+++ README.md
@@ -0,0 +1,14 @@
+# **Shiftlock Module**
+
+#### *Please note that this is a free to use project don't expect a complex code*
+
+* * *
+
+## ***List of functions***
+---
+    module:shiftlock(active) | return active
+active : if this boolean is true, the shiftlock enables. If it's false, it disables shiftlock.
+ 
+
+    module:IsLocked() | return boolean (true for enabled | false for disabled)
+this function tells you if the shiftlock is enabled or not.
---
+++ shiftlockmodule.lua
@@ -0,0 +1,40 @@
+local ShiftlockModule = {}
+
+local UserInputService = game:GetService("UserInputService")
+local RunService = game:GetService("RunService")
+
+local plr = game:GetService("Players").LocalPlayer
+local char = plr.Character or plr.CharacterAdded:Wait()
+local hum = char:WaitForChild("Humanoid")
+local root = char:WaitForChild("HumanoidRootPart")
+local mouse = plr:GetMouse()
+
+function ShiftlockModule:isLocked()
+	if char:GetAttribute("Locked") == true then return false else return true end
+end
+
+function ShiftlockModule:shiftlock(active)
+
+	if active then -- enable shiftlock
+		hum.CameraOffset = Vector3.new(1.75,0,0)
+		hum.AutoRotate = false
+
+		RunService:BindToRenderStep("ShiftLock", Enum.RenderPriority.Character.Value, function()
+			UserInputService.MouseBehavior = Enum.MouseBehavior.LockCenter
+			char:SetAttribute("Locked", true)
+			local _, y = workspace.CurrentCamera.CFrame.Rotation:ToEulerAnglesYXZ()
+			root.CFrame = CFrame.new(root.Position) * CFrame.Angles(0,y,0)
+			mouse.Icon = "rbxassetid://12715007293"
+		end)
+	else -- Disable it
+		hum.AutoRotate = true
+		hum.CameraOffset = Vector3.new(0,0,0)
+		RunService:UnbindFromRenderStep("ShiftLock")
+		UserInputService.MouseBehavior = Enum.MouseBehavior.Default
+		mouse.Icon = ""
+		char:SetAttribute("Locked", false)
+	end
+	return active
+end
+
+return ShiftlockModule
