af01fb43619349910e2eb848be24a11192dfa80a
IamSwan <113911493+IamSwan@users.noreply.github.com>
2023-11-22 19:09:24 +0100

    Update shiftlockmodule.lua

--- shiftlockmodule.lua
+++ shiftlockmodule.lua
@@ -9,21 +9,23 @@ local hum = char:WaitForChild("Humanoid")
 local root = char:WaitForChild("HumanoidRootPart")
 local mouse = plr:GetMouse()
 
-local CUSTOM_SHIFTLOCK_CURSOR = "rbxassetid://0" -- Leave empty for none
+local CUSTOM_SHIFTLOCK_CURSOR = "rbxassetid://0"
+--[[
+	Make sure to replace the 0 by the id of your custom cursor
+]]--
 
 function ShiftlockModule:isLocked()
-	if char:GetAttribute("Locked") == true then return false else return true end
+	return char:GetAttribute("Locked")
 end
 
 function ShiftlockModule:shiftlock(active)
-
 	if active then -- enable shiftlock
 		hum.CameraOffset = Vector3.new(1.75,0,0)
 		hum.AutoRotate = false
 
 		RunService:BindToRenderStep("ShiftLock", Enum.RenderPriority.Character.Value, function()
 			UserInputService.MouseBehavior = Enum.MouseBehavior.LockCenter
-			char:SetAttribute("Locked", true)
+			char:SetAttribute("Locked", active)
 			local _, y = workspace.CurrentCamera.CFrame.Rotation:ToEulerAnglesYXZ()
 			root.CFrame = CFrame.new(root.Position) * CFrame.Angles(0,y,0)
 			mouse.Icon = CUSTOM_SHIFTLOCK_CURSOR
@@ -34,7 +36,7 @@ function ShiftlockModule:shiftlock(active)
 		RunService:UnbindFromRenderStep("ShiftLock")
 		UserInputService.MouseBehavior = Enum.MouseBehavior.Default
 		mouse.Icon = ""
-		char:SetAttribute("Locked", false)
+		char:SetAttribute("Locked", active)
 	end
 	return active
 end
